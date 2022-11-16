966493ba779f607bdd9d3deef4366f9a44e3f497
Eugene Nam jc463628 <54092140+eugenenam111@users.noreply.github.com>
2022-11-16 21:17:08 +1000

    Create snake

--- snake_game.py
+++ snake_game.py
@@ -4,12 +4,18 @@ dis=pygame.display.set_mode((500,500))
 pygame.display.update()
 pygame.display.set_caption("Eugene's Snake Game")
 
+blue=(0,0,255)
+red=(255,0,0)
+
 game_over = False
 while not game_over:
   for event in pygame.event.get():
     print(event)
     if event.type==pygame.QUIT:
       game_over=True
+
+  pygame.draw.rect(dis,blue,[250,250,10,10])
+  pygame.display.update()
 
 
 pygame.quit()
